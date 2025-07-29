d2d834113496ec1166d0254031dcb27cf4e46dee
SylviaRemington <sylviaremington@gmail.com>
2025-07-28 23:21:19 -0700

    adding css to IngredientList.jsx in li to style the ingredient names & changed App.css with white plus for button so more readable for accessibility

--- src/components/IngredientList/IngredientList.jsx
+++ src/components/IngredientList/IngredientList.jsx
@@ -5,7 +5,8 @@ const IngredientList = (props) => {
     <ul>
       {props.ingredients.map((ingredient, index) => (
         // .map can only return one single item, thus button is in li instead of two separate items / they are linked that way
-        <li key={index}>
+        // Note to self: The reason there are two {{ and }} is because - first one tells JSX that writing JS & second is the JS object I'm passing to the style attribute.
+        <li key={index} style={{ backgroundColor: ingredient.color }}>
           {ingredient.name}
           <button onClick={() => props.addToBurger(ingredient)}>+</button>
         </li>
